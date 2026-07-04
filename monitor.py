849a88a65c51d2b58de58076da70896ee283268e
keyttuais <145917480+keyttuais@users.noreply.github.com>
2026-07-04 17:01:56 +0530

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -4,8 +4,6 @@ import random
 import requests
 import re
 from cryptography.fernet import Fernet
-from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
-from cryptography.hazmat.primitives import hashes
 import base64
 
 # --- CONFIG ---
